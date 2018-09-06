5c65b17c2e7f0a4ddf42ac1dfde4adfa21121a41
Rakesh Ranjan <rakeshranjan.568@gmail.com>
2018-09-07 02:18:28 +0530

    Minor

--- src/main.py
+++ src/main.py
@@ -1,13 +1,35 @@
 import json
 import os
 from google.cloud import vision
+import io
 vision_client = vision.ImageAnnotatorClient()
 
-project_id = os.environ['GCP_PROJECT']
+# project_id = os.environ['GCP_PROJECT']
 with open('config.json') as f:
     data = f.read()
 config = json.loads(data)
 
-def detect_text(filename):
-    print('Looking for text in image {}'.format(filename))
-    detect_text(args.path)
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+detect_text('/Users/acko094/Downloads/rc.jpg')
